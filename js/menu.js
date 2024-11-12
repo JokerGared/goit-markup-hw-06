25d793f2f20e07f02d66daa29191e73d57de0324
Oleh <therkasoa@gmail.com>
2024-11-12 15:16:09 +0200

    add new script

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,36 @@
 (() => {
-  const refs = {
-    // Додати атрибут data-modal-open на кнопку відкриття
-    openModalBtn: document.querySelector("[data-menu-open]"),
-    // Додати атрибут data-modal-close на кнопку закриття
-    closeModalBtn: document.querySelector("[data-menu-close]"),
-    // Додати атрибут data-modal на бекдроп модалки
-    modal: document.querySelector("[data-menu]"),
+  // добавили доп класс на контейнер менюшки
+  const mobileMenu = document.querySelector(".js-menu-container");
+  // добавили доп класс на кнопку открывать менюшку
+  const openMenuBtn = document.querySelector(".js-open-menu");
+  // добавили доп класс на кнопку закрывать менюшку
+  const closeMenuBtn = document.querySelector(".js-close-menu");
+  // добавили доп класс что бы закрыть после нажатия на ссылку
+  const menuLinks = document.querySelectorAll(".mobile-modal-link");
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute("aria-expanded") === "true" || false;
+    openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
+    mobileMenu.classList.toggle("is-open");
+
+    const scrollLockMethod = !isMenuOpen
+      ? "disableBodyScroll"
+      : "enableBodyScroll";
+    bodyScrollLock[scrollLockMethod](document.body);
   };
+  //  добавили что бы закрывалось при нажатии на ссылку
+  menuLinks.forEach((menuLink) => {
+    menuLink.addEventListener("click", toggleMenu);
+  });
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
-    refs.modal.classList.toggle("is-open");
-  }
+  // стандартно из скрипта от Репеты
+  openMenuBtn.addEventListener("click", toggleMenu);
+  closeMenuBtn.addEventListener("click", toggleMenu);
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove("is-open");
+    openMenuBtn.setAttribute("aria-expanded", false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
 })();
